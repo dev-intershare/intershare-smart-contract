2c7e3c14821796f536f3e3636753dbc506b10c5d
Willie Schoeman <schoemanwil@gmail.com>
2026-02-19 08:08:16 +0200

    Added more functionality to the USDT swapping contract

--- src/gateways/IS21USDTSwappingGateway.sol
+++ src/gateways/IS21USDTSwappingGateway.sol
@@ -8,6 +8,7 @@ import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 /**
  * @title IS21USDTSwappingGateway
@@ -19,24 +20,29 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
  *  - pendingMintUSDT  (user deposits, fund managers withdraw)
  *  - availableBurnUSDT (fund managers deposit, users withdraw)
  */
+
+struct FiatReserves {
+    bytes32 currency;
+    uint256 amount;
+}
+
 interface IIS21FundManagerGateway {
-    function mintWithReserveIncrease(
+    function mintWithReservesIncrease(
         address to,
-        bytes32 currency,
-        uint256 reserveIncrease,
+        FiatReserves[] calldata reserveIncreases,
         uint256 mintAmount
     ) external;
 
-    function burnWithReserveDecrease(
+    function burnWithReservesDecrease(
         address from,
-        bytes32 currency,
-        uint256 reserveDecrease,
+        FiatReserves[] calldata reserveDecreases,
         uint256 burnAmount
     ) external;
 }
 
 contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
     ///////////////
     // Errors    //
@@ -49,6 +55,7 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     error IS21SG__InsufficientBurnLiquidity();
     error IS21SG__ZeroAddress();
     error IS21SG__InvalidWallet();
+    error IS21SG__OnlyFundManagerCanExecute();
 
     //////////////////
     // Structs      //
@@ -73,6 +80,8 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     // Constants       //
     /////////////////////
     bytes32 private constant CURRENCY_USDT = bytes32("USDT");
+    bytes32 private constant ACTION_MINT = keccak256("MINT");
+    bytes32 private constant ACTION_BURN = keccak256("BURN");
     bytes32 private constant MINT_TYPEHASH =
         keccak256(
             "MintQuote(address wallet,uint256 usdtAmount,uint256 is21Amount,uint256 nonce,uint256 expiry)"
@@ -90,6 +99,7 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     IERC20 public immutable usdt;
     IIS21FundManagerGateway public immutable fundManagerGateway;
     address public immutable trustedSigner;
+    EnumerableSet.AddressSet private sFundManagers;
 
     // --- Liquidity accounting ---
     uint256 public pendingMintUSDT;
@@ -117,9 +127,15 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
 
     event BurnUSDTDeposited(uint256 amount, uint256 timestamp);
     event MintUSDTWithdrawn(uint256 amount, uint256 timestamp);
-
     event GatewayPaused(address indexed caller, uint256 timestamp);
     event GatewayUnpaused(address indexed caller, uint256 timestamp);
+    event FundManagerApproved(address indexed caller, uint256 timestamp);
+    event FundManagerRevoked(address indexed caller, uint256 timestamp);
+    event NonceUsed(
+        address indexed user,
+        uint256 indexed nonce,
+        bytes32 action
+    );
 
     ///////////////
     // Modifiers //
@@ -134,6 +150,13 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     modifier nonZeroAddress(address to) {
         if (to == address(0)) {
             revert IS21SG__NotZeroAddress();
+        }
+        _;
+    }
+
+    modifier onlyFundManager() {
+        if (!sFundManagers.contains(msg.sender)) {
+            revert IS21SG__OnlyFundManagerCanExecute();
         }
         _;
     }
@@ -198,20 +221,28 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
             )
         );
 
-        if (ECDSA.recover(digest, signature) != trustedSigner)
+        if (ECDSA.recover(digest, signature) != trustedSigner) {
             revert IS21SG__InvalidSignature();
+        }
 
         sUsedNonces[msg.sender][quote.nonce] = true;
+        emit NonceUsed(msg.sender, quote.nonce, ACTION_MINT);
 
         // --- Custody USDT ---
         usdt.safeTransferFrom(msg.sender, address(this), quote.usdtAmount);
         pendingMintUSDT += quote.usdtAmount;
 
         // --- Mint through Fund Manager Gateway ---
-        fundManagerGateway.mintWithReserveIncrease(
+        FiatReserves[] memory reserves = new FiatReserves[](1);
+
+        reserves[0] = FiatReserves({
+            currency: CURRENCY_USDT,
+            amount: quote.usdtAmount
+        });
+
+        fundManagerGateway.mintWithReservesIncrease(
             msg.sender,
-            CURRENCY_USDT,
-            quote.usdtAmount,
+            reserves,
             quote.is21Amount
         );
 
@@ -263,18 +294,27 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
             )
         );
 
-        if (ECDSA.recover(digest, signature) != trustedSigner)
+        if (ECDSA.recover(digest, signature) != trustedSigner) {
             revert IS21SG__InvalidSignature();
+        }
 
         // Lock nonce - used
         sUsedNonces[msg.sender][quote.nonce] = true;
+        emit NonceUsed(msg.sender, quote.nonce, ACTION_BURN);
 
         availableBurnUSDT -= quote.usdtAmount;
 
-        fundManagerGateway.burnWithReserveDecrease(
+        // --- Burn through Fund Manager Gateway ---
+        FiatReserves[] memory reserves = new FiatReserves[](1);
+
+        reserves[0] = FiatReserves({
+            currency: CURRENCY_USDT,
+            amount: quote.usdtAmount
+        });
+
+        fundManagerGateway.burnWithReservesDecrease(
             msg.sender,
-            CURRENCY_USDT,
-            quote.usdtAmount,
+            reserves,
             quote.is21Amount
         );
 
@@ -294,7 +334,7 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     function withdrawMintUSDT(
         address to,
         uint256 amount
-    ) external onlyOwner nonReentrant {
+    ) external onlyFundManager whenNotPaused nonReentrant {
         if (amount == 0 || amount > pendingMintUSDT)
             revert IS21SG__InvalidAmount();
 
@@ -304,7 +344,9 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
         emit MintUSDTWithdrawn(amount, block.timestamp);
     }
 
-    function depositBurnUSDT(uint256 amount) external onlyOwner nonReentrant {
+    function depositBurnUSDT(
+        uint256 amount
+    ) external onlyFundManager whenNotPaused nonReentrant {
         if (amount == 0) revert IS21SG__InvalidAmount();
 
         usdt.safeTransferFrom(msg.sender, address(this), amount);
@@ -314,8 +356,46 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
     }
 
     ///////////////////////////////////
-    // Admin Controls                 //
+    // Admin Controls                //
     ///////////////////////////////////
+    /**
+        This function allows the owner to approve a fund manager.
+        @param fundManager The address of the fund manager to be approved.
+        @notice This function can only be called by the current owner and is protected against reentrancy attacks.
+        @dev It updates the mapping of approved fund managers and emits an event for tracking purposes.
+    */
+    function approveFundManager(
+        address fundManager
+    ) external onlyOwner nonZeroAddress(fundManager) nonReentrant {
+        if (sFundManagers.add(fundManager)) {
+            emit FundManagerApproved(fundManager, block.timestamp);
+        }
+    }
+
+    /** 
+        This function allows the owner to revoke a fund manager's approval.
+        @param fundManager The address of the fund manager to be revoked.
+        @notice This function can only be called by the current owner and is protected against reentrancy attacks.
+        @dev It updates the mapping of approved fund managers and emits an event for tracking purposes.
+    */
+    function revokeFundManager(
+        address fundManager
+    ) external onlyOwner nonZeroAddress(fundManager) nonReentrant {
+        if (sFundManagers.remove(fundManager)) {
+            emit FundManagerRevoked(fundManager, block.timestamp);
+        }
+    }
+
+    /**
+        This function retrieves the list of all approved fund managers.
+        @return An array of addresses representing the approved fund managers.
+        @notice This function is view-only and does not modify the state of the contract.
+        @dev It can be used to get a list of all fund managers for administrative or informational purposes.
+    */
+    function getFundManagers() external view returns (address[] memory) {
+        return sFundManagers.values();
+    }
+
     function pause() external onlyOwner {
         _pause();
         emit GatewayPaused(msg.sender, block.timestamp);
@@ -334,5 +414,9 @@ contract IS21USDTSwappingGateway is EIP712, Ownable, Pausable, ReentrancyGuard {
         uint256 nonce
     ) external view returns (bool) {
         return sUsedNonces[user][nonce];
+    }
+
+    function getFundManagerGateway() external view returns (address) {
+        return address(fundManagerGateway);
     }
 }
